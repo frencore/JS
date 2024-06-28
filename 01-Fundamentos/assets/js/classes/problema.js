e5fe006e8b6915122acda5ad6ef62b376beed610
Felipe Rencoret. <felipe.antonio.rencoret@gmail.com>
2024-06-28 17:27:35 -0400

    se agregan correccciones: problema.js

--- 01-Fundamentos/assets/js/classes/problema.js
+++ 01-Fundamentos/assets/js/classes/problema.js
@@ -15,31 +15,28 @@ const pedro = {
     nombre: 'Pedro',
     edad: 15,
     imprimir() {
-        //como no esta en el mismo scope o nivel no es bueno usar fher.nombre por temas de mentencion 
-        console.log(`Nombre: ${fher.nombre}`);
-        //mejor usa this para hacer referencia al nivel superior 
         console.log(`Nombre: ${this.nombre} - edad ${this.edad}`);
     }
 };
 
 // fher.imprimir();
+//uso de clases anteriormente
 function Persona(nombre, edad) {
 
-    console.log('se ejecuto esta linea');
-    this.nomre = nombre;
+    this.nombre = nombre;
     this.edad = edad;
     this.imprimir = function () {
-        //como no esta en el mismo scope o nivel no es bueno usar fher.nombre por temas de mentencion 
-        console.log(`Nombre: ${fher.nombre}`);
         //mejor usa this para hacer referencia al nivel superior 
         console.log(`Nombre: ${this.nombre} - edad ${this.edad}`);
     };
-
 };
+
+
 //nueva instancia de Persona
 const maria = new Persona('Maria', 18);
+console.log(maria); 
 const melisa = new Persona('Melisa', 35);
-console.log(maria);
+
 
 maria.imprimir();
 melisa.imprimir();
