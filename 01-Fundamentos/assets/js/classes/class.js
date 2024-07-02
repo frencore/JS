863c539ff25462b6015d6965e819baf5b13929cf
Felipe Rencoret. <felipe.antonio.rencoret@gmail.com>
2024-07-02 15:31:15 -0400

    propiedades estaticas: class.js

--- 01-Fundamentos/assets/js/classes/class.js
+++ 01-Fundamentos/assets/js/classes/class.js
@@ -3,16 +3,26 @@
 
 class Persona {
 
+
+    static _conteo = 0;
+
+    static get conteo() {
+        return Persona._conteo + ' instancias';
+    }
+
+
     nombre = '';
     codigo = '';
     frase = '';
     comida = '';
 
     constructor(nombre = 'Sin nombre', codigo = 'Sin codigo', frase = 'Sin frase') {
-        // console.log('Hola!');
+
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
+
+        Persona._conteo++;
     }
 
     set setComidaFavorita(comida) {
@@ -35,6 +45,7 @@ class Persona {
 const spiderman = new Persona('Peter Parker', 'Spider', 'spiderman');
 const ironman = new Persona('Tony Stark', 'Ironman', 'yo soy Ironman');
 
+
 //console.log(ironman);
 
 //spiderman.quienSoy();
@@ -43,6 +54,9 @@ spiderman.miFrase();
 
 spiderman.setComidaFavorita = 'El pie de cereza';
 
-console.log(spiderman.getComidaFavorita);
+// console.log(spiderman.getComidaFavorita);
 
-console.log(spiderman);
+// console.log(spiderman);
+
+console.log('Conteo statico', Persona._conteo);
+console.log(Persona.conteo);
